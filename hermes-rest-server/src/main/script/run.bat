d1b14ffb6d886899bbd0dea60f74be041471dcaf
orce-mage <nikolovorce2@gmail.com>
2014-10-31 12:28:01 +0000

    Fixes for Daniels environment.

--- hermes-rest-server/src/main/script/run.bat
+++ hermes-rest-server/src/main/script/run.bat
@@ -1,1 +1,2 @@
-java -Dlogback.configurationFile=logback.xml -Dhermes.config=hermes.properties -Xmx512m -Xms512m -jar hermes-rest-server-1.0.0-SNAPSHOT-shaded.jar --port "8080" --jersey "/api/*" --cors "/api/*"
+"C:\Program Files\Java\jre1.8.0_25\bin\java.exe" -Dlogback.configurationFile=logback.xml -Dhermes.config=hermes.properties -Xmx512m -Xms512m -jar hermes-rest-server-1.0.0-SNAPSHOT-shaded.jar --port "8080" --jersey "/api/*" --cors "/api/*"
+pause
