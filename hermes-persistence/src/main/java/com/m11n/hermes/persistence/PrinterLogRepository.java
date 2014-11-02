0b8f1fe2c2b12daf373f3316e3fe99e391ba9cd5
orce-mage <nikolovorce2@gmail.com>
2014-11-02 19:36:29 +0000

    Printer log REST implemented.

--- hermes-persistence/src/main/java/com/m11n/hermes/persistence/PrinterLogRepository.java
+++ hermes-persistence/src/main/java/com/m11n/hermes/persistence/PrinterLogRepository.java
@@ -1,10 +1,10 @@
 package com.m11n.hermes.persistence;
 
 import com.m11n.hermes.core.model.PrinterLog;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PrinterLogRepository extends CrudRepository<PrinterLog, String> {
+public interface PrinterLogRepository extends PagingAndSortingRepository<PrinterLog, String> {
     PrinterLog findByOrderId(String orderId);
 }
--- hermes-rest-api/src/main/java/com/m11n/hermes/rest/api/PrinterLogResource.java
+++ hermes-rest-api/src/main/java/com/m11n/hermes/rest/api/PrinterLogResource.java
@@ -1,11 +1,13 @@
 package com.m11n.hermes.rest.api;
 
+import com.m11n.hermes.persistence.PrinterLogRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.inject.Inject;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -15,11 +17,12 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 public class PrinterLogResource {
     private static final Logger logger = LoggerFactory.getLogger(PrinterLogResource.class);
 
+    @Inject
+    private PrinterLogRepository printerLogRepository;
+
     @GET
     @Produces(APPLICATION_JSON)
-    public Response list() throws Exception {
-        // TODO: implement this
-
-        return Response.ok().build();
+    public Response list(@QueryParam("page") @DefaultValue("0") int page, @QueryParam("size") @DefaultValue("20") int size) throws Exception {
+        return Response.ok(printerLogRepository.findAll(new PageRequest(page, size))).build();
     }
 }
