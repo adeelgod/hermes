de1de515efdf57165462b068b292f921f247a7aa
orce-mage <nikolovorce2@gmail.com>
2014-11-02 09:44:31 +0000

    Added persistence configuration.

--- hermes-persistence/src/main/java/com/m11n/hermes/persistence/PrinterLogRepository.java
+++ hermes-persistence/src/main/java/com/m11n/hermes/persistence/PrinterLogRepository.java
@@ -2,6 +2,8 @@ package com.m11n.hermes.persistence;
 
 import com.m11n.hermes.core.model.PrinterLog;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PrinterLogRepository extends CrudRepository<PrinterLog, String> {
 }
