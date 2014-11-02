124073088837824c763e285a708abb606d6469d5
orce-mage <nikolovorce2@gmail.com>
2014-11-02 10:09:20 +0000

    Added external configuration.

--- hermes-rest-server/run.sh
+++ hermes-rest-server/run.sh
@@ -9,10 +9,6 @@ then
 fi
 
 export HERMES_CONFIG_BASE=file:src/test/resources
-export HERMES_CONFIG=\
-"$HERMES_CONFIG_BASE/hermes.common.properties,\
-$HERMES_CONFIG_BASE/hermes.database.properties,\
-$HERMES_CONFIG_BASE/hermes.security.properties,\
-$HERMES_CONFIG_BASE/hermes.esb.properties"
+export HERMES_CONFIG="$HERMES_CONFIG_BASE/hermes.properties"
 #java -Dlogback.configurationFile=$HERMES_CONFIG_BASE/logback.xml -Dhermes.config="$HERMES_CONFIG" $JPDA_OPTS -Xmx1g -Xms1g -Xss64m -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -jar target/hermes-rest-server-1.0.0-SNAPSHOT-shaded.jar --port "8080" --security "/*" --jersey "/api/*" --cors "/api/*"
 java -Dlogback.configurationFile=$HERMES_CONFIG_BASE/logback.xml -Dhermes.config="$HERMES_CONFIG" $JPDA_OPTS -Xmx1g -Xms1g -Xss64m -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -jar target/hermes-rest-server-1.0.0-SNAPSHOT-shaded.jar --port "8080" --jersey "/api/*" --cors "/api/*"
