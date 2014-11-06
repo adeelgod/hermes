6e55e42eae985c77829bdcf3cb326a8f768a3d11
orce-mage <nikolovorce2@gmail.com>
2014-11-06 12:01:54 +0000

    Using default values for form fields.

--- hermes-persistence/src/main/java/com/m11n/hermes/persistence/util/DataInitializer.java
+++ hermes-persistence/src/main/java/com/m11n/hermes/persistence/util/DataInitializer.java
@@ -19,13 +19,13 @@ public class DataInitializer {
     public void init() {
         Form form = new Form();
         form.setName("orders");
-        form.setSqlStatement("select * from orders");
+        form.setSqlStatement("SELECT `order_id`, `Bestellung`, concat(\"Bestellung: \",`Bestellung`),`shipping_lable` FROM `mage_custom_order` WHERE `Status` = \"complete\" and `Datum_Lieferung` > \":from\" and `Datum_Lieferung` < \":until\"");
         form = formRepository.save(form);
 
         List<FormField> fields = new ArrayList<>();
         fields.add(new FormField("from", FormField.Type.DATE.name(), 1));
         fields.add(new FormField("until", FormField.Type.DATE.name(), 2));
-        fields.add(new FormField("dummy_boolean", FormField.Type.BOOLEAN.name(), 3));
+        fields.add(new FormField("dummy_boolean", FormField.Type.BOOLEAN.name(), 3, "false"));
         fields.add(new FormField("dummy_text", FormField.Type.TEXT.name(), 4));
 
         form.setFields(fields);
