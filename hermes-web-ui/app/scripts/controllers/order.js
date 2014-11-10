272bf0d6002043442d8e6fa64690fec335052506
orce-mage <nikolovorce2@gmail.com>
2014-11-10 16:08:06 +0000

    Selectable orders. UI improvements.

--- hermes-web-ui/app/scripts/controllers/order.js
+++ hermes-web-ui/app/scripts/controllers/order.js
@@ -20,36 +20,26 @@ angular.module('hermes.ui').controller('OrderCtrl', function ($scope, $alert, Pr
     $scope.query = function() {
         $scope.params['_form'] = $scope.configuration['hermes.orders.form'];
         FormSvc.query($scope.params).success(function(data) {
-            $scope.orderstmp = data;
-        });
-    };
-
-    // deprecated
-    $scope.search = function() {
-        PrinterLogSvc.list({page: 0, from: $scope.fromDate, until: $scope.untilDate}).success(function(data) {
             $scope.orders = data;
         });
     };
 
-    // deprecated
-    $scope.select = function() {
-        PrinterLogSvc.select({from: $scope.fromDate, until: $scope.untilDate, selected: true}).success(function(data) {
-            $alert({content: 'Entries selected: ' + data, placement: 'top', type: 'success', show: true, duration: 3});
-            $scope.search();
-        });
-    };
-
-    // deprecated
-    $scope.unselect = function() {
-        PrinterLogSvc.select({from: $scope.fromDate, until: $scope.untilDate, selected: false}).success(function(data) {
-            //$scope.selected = data;
-            $alert({content: 'Entries unselected: ' + data, placement: 'top', type: 'success', show: true, duration: 3});
-            $scope.search();
+    $scope.select = function(selected) {
+        angular.forEach($scope.orders, function(order) {
+            order._selected = selected;
         });
     };
 
     // deprecated
     $scope.print = function() {
+        // TODO: implement this
+        $alert({content: 'Print scheduled', placement: 'top', type: 'success', show: true, duration: 3});
+        angular.forEach($scope.orders, function(order) {
+            if(order._selected) {
+                $alert({content: 'Printing order: ' + order.orderId, placement: 'top', type: 'success', show: true, duration: 3});
+            }
+        });
+        /**
         $scope.printing = true;
         PrinterLogSvc.print().success(function(data) {
             $alert({content: 'Print scheduled', placement: 'top', type: 'success', show: true, duration: 3});
@@ -58,6 +48,7 @@ angular.module('hermes.ui').controller('OrderCtrl', function ($scope, $alert, Pr
             $alert({content: 'Print failed', placement: 'top', type: 'danger', show: true, duration: 3});
             $scope.printing = false;
         });
+         */
     };
 
     $scope.pages = function() {
