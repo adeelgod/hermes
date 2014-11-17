c9e6ebb8116666a3f5ac6c2b8f8c989a7c1d4794
orce-mage <nikolovorce2@gmail.com>
2014-11-17 09:22:38 +0000

    Minor improvement JSON.

--- hermes-core/src/main/java/com/m11n/hermes/core/model/DocumentType.java
+++ hermes-core/src/main/java/com/m11n/hermes/core/model/DocumentType.java
@@ -1,5 +1,8 @@
 package com.m11n.hermes.core.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape= JsonFormat.Shape.OBJECT)
 public enum DocumentType {
     INVOICE, LABEL, REPORT
 }
