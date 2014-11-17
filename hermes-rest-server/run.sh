3250045c5726a342b549de0bac642eed61148cef
orce-mage <nikolovorce2@gmail.com>
2014-11-17 10:43:47 +0000

    Fixed new unified test configuration.

--- hermes-rest-server/run.sh
+++ hermes-rest-server/run.sh
@@ -8,7 +8,7 @@ then
     export JPDA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
 fi
 
-export HERMES_CONFIG_BASE=file:src/test/resources
+export HERMES_CONFIG_BASE=file:../hermes-test/src/main/resources
 export HERMES_CONFIG="$HERMES_CONFIG_BASE/hermes.properties"
 #java -Dlogback.configurationFile=$HERMES_CONFIG_BASE/logback.xml -Dhermes.config="$HERMES_CONFIG" $JPDA_OPTS -Xmx1g -Xms1g -Xss64m -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -jar target/hermes-rest-server-1.0.0-SNAPSHOT-shaded.jar --port "8080" --security "/*" --jersey "/api/*" --cors "/api/*"
 java -Dlogback.configurationFile=$HERMES_CONFIG_BASE/logback.xml -Dhermes.config="$HERMES_CONFIG" $JPDA_OPTS -Xmx1g -Xms1g -Xss64m -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -jar target/hermes-rest-server-1.0.0-SNAPSHOT-shaded.jar --port "8080" --jersey "/api/*" --cors "/api/*"
