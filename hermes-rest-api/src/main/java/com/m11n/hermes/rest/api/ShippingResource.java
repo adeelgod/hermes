5abae72c10fcbfc74dcf9d6ea2758600a19a2296
orce-mage <nikolovorce2@gmail.com>
2014-11-25 22:25:33 +0000

    First shot status implementation.

--- hermes-rest-api/src/main/java/com/m11n/hermes/rest/api/ShippingResource.java
+++ hermes-rest-api/src/main/java/com/m11n/hermes/rest/api/ShippingResource.java
@@ -1,6 +1,9 @@
 package com.m11n.hermes.rest.api;
 
+import com.m11n.hermes.core.model.LabelStatus;
+import com.m11n.hermes.persistence.AuswertungRepository;
 import com.m11n.hermes.persistence.LabelStatusRepository;
+import com.m11n.hermes.persistence.SalesFlatShipmentCommentRepository;
 import com.m11n.hermes.persistence.util.QueryScheduler;
 import com.m11n.hermes.service.magento.MagentoServiceLocator;
 import org.slf4j.Logger;
@@ -12,6 +15,7 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -27,6 +31,12 @@ public class ShippingResource {
 
     @Inject
     private LabelStatusRepository labelStatusRepository;
+
+    @Inject
+    private SalesFlatShipmentCommentRepository salesFlatShipmentCommentRepository;
+
+    @Inject
+    private AuswertungRepository auswertungRepository;
 
     private String username;
 
@@ -56,8 +66,12 @@ public class ShippingResource {
     @GET
     @Path("/status")
     @Produces(APPLICATION_JSON)
-    public Response status() {
-        // TODO: implement this
-        return Response.ok().build();
+    public Response status(@QueryParam("orderId") String orderId, @QueryParam("pos") int pos) {
+        // TODO: check this
+        String shippingId = auswertungRepository.findShippingIdByOrderId(orderId);
+        String rawStatus = salesFlatShipmentCommentRepository.findRawStatus(shippingId, pos);
+        LabelStatus status = labelStatusRepository.findByText(rawStatus);
+
+        return Response.ok(status).build();
     }
 }
