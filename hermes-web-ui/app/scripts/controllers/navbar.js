5ad6c6e653b1111d400658feb592b14f74b775cb
orce-mage <nikolovorce2@gmail.com>
2014-11-25 11:39:36 +0000

    Lint fix.

--- hermes-web-ui/app/scripts/controllers/navbar.js
+++ hermes-web-ui/app/scripts/controllers/navbar.js
@@ -3,12 +3,12 @@
 angular.module('hermes.ui').controller('NavbarCtrl', function ($scope, ExampleSvc) {
     $scope.dropdown = [
         {
-            "text": "<i class=\"fa fa-pencil\"></i>&nbsp;Edit",
-            "href": "#!/form/edit"
+            'text': '<i class=\"fa fa-pencil\"></i>&nbsp;Edit',
+            'href': "#!/form/edit"
         },
         {
-            "text": "<i class=\"fa fa-pencil\"></i>&nbsp;List",
-            "href": "#!/form/list"
+            "text": '<i class=\"fa fa-pencil\"></i>&nbsp;List',
+            "href": '#!/form/list'
         }
     ];
 
