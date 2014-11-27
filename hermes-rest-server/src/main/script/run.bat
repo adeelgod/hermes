a1b18a1288f59c51cc62a2b25f5611c9c2705f67
orce-mage <nikolovorce2@gmail.com>
2014-11-27 09:24:09 +0000

    Fixed low memory configuration. Fixed assembly (removed version from final JAR file).

--- hermes-rest-server/src/main/script/run.bat
+++ hermes-rest-server/src/main/script/run.bat
@@ -1,2 +1,3 @@
-"C:\Program Files\Java\jre1.8.0_25\bin\java.exe" -Dlogback.configurationFile=logback.xml -Dhermes.config=hermes.properties -Xmx512m -Xms512m -jar hermes-rest-server-1.0.0-SNAPSHOT-shaded.jar --port "8080" --jersey "/api/*" --cors "/api/*"
+"C:\Program Files\Java\jre1.8.0_25\bin\java.exe" -Dlogback.configurationFile=logback.xml -Dhermes.config=hermes.properties -Xmx1024m -Xms512m -jar hermes-rest-server.jar --port "8080" --jersey "/api/*" --cors "/api/*"
+REM "C:\Program Files (x86)\Java\jre1.8.0_25\bin\java.exe" -Dlogback.configurationFile=logback.xml -Dhermes.config=hermes.properties -Xmx1024m -Xms512m -jar hermes-rest-server.jar --port "8080" --jersey "/api/*" --cors "/api/*"
 pause
