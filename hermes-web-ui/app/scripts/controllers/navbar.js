c9c7241be327bf16210a812948bfc54bc2611567
orce-mage <nikolovorce2@gmail.com>
2014-11-28 05:12:13 +0000

    Major UI cleanup. Streamlined buttons. Added tooltips.

--- hermes-web-ui/app/scripts/controllers/navbar.js
+++ hermes-web-ui/app/scripts/controllers/navbar.js
@@ -15,6 +15,19 @@ angular.module('hermes.ui').controller('NavbarCtrl', function ($scope, ExampleSv
         }
     ];
 
+    $scope.tooltips = {
+        help: {title: 'Help', placement: 'bottom', type: 'info'},
+        orders: {title: 'Search orders...', placement: 'bottom', type: 'info'},
+        shipping: {title: 'Search shippings...', placement: 'bottom', type: 'info'},
+        execute: {title: 'Print reports...', placement: 'bottom', type: 'info'},
+        examples: {title: 'Copy example PDFs...', placement: 'bottom', type: 'info'},
+        printers: {title: 'Show printers...', placement: 'bottom', type: 'info'},
+        forms: {title: 'Edit forms and queries...', placement: 'bottom', type: 'info'},
+        configuration: {title: 'Change configuration...', placement: 'bottom', type: 'info'},
+        signin: {title: 'Login...', placement: 'bottom', type: 'info'},
+        logout: {title: 'Logout...', placement: 'bottom', type: 'info'}
+    };
+
     $scope.list = function() {
         $scope.loading = true;
         $scope.forms = [];
