54649260969b33a9d56645d91d6aa8c39d52c463
orce-mage <nikolovorce2@gmail.com>
2014-12-02 16:34:52 +0000

    chg: dev: Added Spring configuration.

--- hermes-service-magento/src/test/java/com/m11n/hermes/service/magento/MagentoServiceTest.java
+++ hermes-service-magento/src/test/java/com/m11n/hermes/service/magento/MagentoServiceTest.java
@@ -1,18 +1,29 @@
 package com.m11n.hermes.service.magento;
 
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@FixMethodOrder
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:META-INF/spring/applicationContext-hermes.xml"})
 public class MagentoServiceTest {
 
     private static final Logger logger = LoggerFactory.getLogger(MagentoServiceTest.class);
 
     private Mage_Api_Model_Server_V2_HandlerPortType magentoService;
 
-    private String username = "print2";
-    private String password = "edgtds45";
+    @Value("${hermes.magento.api.username}")
+    private String username;
+
+    @Value("${hermes.magento.api.password}")
+    private String password;
 
     @Before
     public void setUp() throws Exception {
