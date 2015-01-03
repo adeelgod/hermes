edeaae4a19e82422a7da85aea4e1d15b599c0c63
orce-mage <nikolovorce2@gmail.com>
2015-01-03 04:07:20 +0000

    fix: Fixed column name.

--- hermes-persistence/src/main/resources/filter.sql
+++ hermes-persistence/src/main/resources/filter.sql
@@ -1,5 +1,5 @@
 SELECT
-  a.Bestellung as orderId, a.GesamtPreis_der_Bestellung_Brutto as amount, a.Datum_Kauf as dateTransfer, a.typ as type, a.Status as status, a.Kunden_ebay_name as ebayName, a.Kunden_vorname as firstname, a.Kunden_name as lastname
+  a.Bestellung as orderId, a.GesamtPreis_der_Bestellung_Brutto as amount, a.Datum_Kauf as orderDate, a.typ as type, a.Status as status, a.Kunden_ebay_name as ebayName, a.Kunden_vorname as firstname, a.Kunden_name as lastname
 FROM
   mage_custom_order AS a,
   bank_statement AS b
