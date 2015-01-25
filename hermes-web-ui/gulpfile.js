bc38c32563164019b7311e02f6003cb8241bc7dd
orce-mage <nikolovorce2@gmail.com>
2015-01-25 02:00:31 +0000

    fix: Fix bower_components location (should not be in app directory fix: Fix dependency on runSequence in gulpfile.js fix: Remove separate html2js task (integrated with jade now) fix: Fix paths in *.less files

--- hermes-web-ui/gulpfile.js
+++ hermes-web-ui/gulpfile.js
@@ -1,31 +1,14 @@
+/* global require */
 'use strict';
 
 var gulp = require('gulp');
-var runSequence = require('run-sequence');
-var gettext = require('gulp-angular-gettext');
 
 // load plugins
 var $ = require('gulp-load-plugins')();
 
-process.NODE_ENV = 'test';
+gulp.task('translations', ['pot'], function () {
+    var gettext = require('gulp-angular-gettext');
 
-// error handler
-// NOTE: see here explanation http://cameronspear.com/blog/how-to-handle-gulp-watch-errors-with-plumber/
-var onError = function (err) {
-    //gutil.beep();
-    console.error(err);
-    throw err;
-};
-
-gulp.task('pot', function () {
-    return gulp.src(['.tmp/**/*.html', '.tmp/scripts/**/*.js', 'app/scripts/**/*.js'])
-        .pipe(gettext.extract('template.pot', {
-            // options to pass to angular-gettext-tools...
-        }))
-        .pipe(gulp.dest('app/po/'));
-});
-
-gulp.task('translations', function () {
     return gulp.src('app/po/**/*.po')
         .pipe(gettext.compile({
             // options to pass to angular-gettext-tools...
@@ -34,76 +17,20 @@ gulp.task('translations', function () {
         .pipe(gulp.dest('app/data/translations/'));
 });
 
-// converts templates to Javascript
-gulp.task('html2js', function () {
-    return gulp.src([".tmp/views/*.html"])
-        .pipe($.minifyHtml({
-            empty: true,
-            spare: true,
-            quotes: true
+gulp.task('pot', function () {
+    var gettext = require('gulp-angular-gettext');
+
+    return gulp.src(['.tmp/**/*.html', 'app/scripts/**/*.js'])
+        .pipe(gettext.extract('template.pot', {
+            // options to pass to angular-gettext-tools...
         }))
-        .pipe($.ngHtml2js({
-            moduleName: "hermes.ui.views",
-            prefix: "views/"
-        }))
-        .pipe($.concat("views.tpl.js"))
-        .pipe(gulp.dest(".tmp/scripts"))
-        .pipe($.size());
+        .pipe(gulp.dest('app/po/'));
 });
 
-// converts markdown to HTML
-gulp.task('markdown', function () {
-    return gulp.src(['app/content/**/*.md'])
-        .pipe($.markdown())
-        .pipe($.minifyHtml({
-            empty: true,
-            spare: true,
-            quotes: true
-        }))
-        .pipe(gulp.dest('.tmp/content'))
-        .pipe($.size());
-});
-
-gulp.task('jade', ['markdown'], function () {
-    // NOTE: pretty option is extremely important; otherwise the html pipeline breaks
-    return gulp.src(['app/**/*.jade', '!app/layouts/**/*.jade', '!app/bower_components/**/*.jade'])
-        .pipe($.jade({
-            basedir: 'app',
-            pretty: true,
-            data: {
-                debug: false
-            }
-        }))
-        .pipe($.replace('@@iTunesId', 'TODO_ITUNES_ID'))
-        .pipe($.replace('@@googleSiteVerification', 'TODO_GOOGLE_SITE_VERIFICATION'))
-        .pipe(gulp.dest('.tmp'));
-});
-
-gulp.task('styles', function () {
-    return gulp.src('app/styles/main.less')
-        .pipe($.less())
-        .pipe($.autoprefixer('last 1 version'))
-        .pipe(gulp.dest('.tmp/styles'))
-        .pipe($.size());
-});
-
-// Scripts
-gulp.task('scripts', function () {
-    return gulp.src('app/scripts/**/*.js')
-        .pipe($.jshint())
-        .pipe($.jshint.reporter(require('jshint-stylish')))
-        .pipe($.ngAnnotate())
-        .pipe(gulp.dest('.tmp/scripts'))
-        .pipe($.size());
-});
-
-gulp.task('html', function () {
+gulp.task('html', ['jade'], function () {
     var assets = $.useref.assets();
 
     return gulp.src(['.tmp/**/*.html'])
-        .pipe($.plumber({
-            errorHandler: onError
-        }))
         .pipe(assets)
         .pipe($.if('*.js', $.sourcemaps.init()))
         .pipe($.if('*.js', $.uglify()))
@@ -116,8 +43,76 @@ gulp.task('html', function () {
         .pipe($.if('*.css', $.rev()))
         .pipe($.useref())
         .pipe($.revReplace())
-        .pipe(gulp.dest('dist'))
-        .pipe($.size());
+        .pipe(gulp.dest('dist'));
+});
+
+// Scripts
+gulp.task('scripts', function () {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe($.jshint())
+        .pipe($.jshint.reporter(require('jshint-stylish')))
+        .pipe($.ngAnnotate({
+            remove: true,
+            add: true,
+            single_quotes: true
+        }))
+        .pipe(gulp.dest('.tmp/scripts'));
+});
+
+gulp.task('jade', ['markdown', 'scripts', 'styles'], function () {
+    var bowerFiles = require('main-bower-files');
+
+    // NOTE: pretty option is extremely important; otherwise the html pipeline breaks
+    return gulp.src(['app/**/*.jade', '!app/layouts/**/*.jade'])
+        .pipe($.jade({
+            basedir: 'app',
+            pretty: true,
+            data: {
+                debug: false
+            }
+        }))
+        .pipe($.replace('@@iTunesId', 'TODO_ITUNES_ID'))
+        .pipe($.replace('@@googleSiteVerification', 'TODO_GOOGLE_SITE_VERIFICATION'))
+        .pipe($.inject(
+            gulp.src(bowerFiles(), {read: false}),
+            {name: 'vendor', relative: true}
+        ))
+        .pipe($.inject(
+            gulp.src('.tmp/**/*.js', {read: false}).pipe($.angularFilesort()),
+            {name: 'angular', relative: true}
+        ))
+        .pipe(gulp.dest('.tmp'))
+        .pipe($.filter(['views/**/*.html']))
+        .pipe($.minifyHtml({
+            empty: true,
+            spare: true,
+            quotes: true
+        }))
+        .pipe($.ngHtml2js({
+            moduleName: "hermes.ui.views",
+            prefix: "views/"
+        }))
+        .pipe($.concat("views.tpl.js"))
+        .pipe(gulp.dest(".tmp/scripts"));
+});
+
+// converts markdown to HTML
+gulp.task('markdown', function () {
+    return gulp.src(['app/content/**/*.md'])
+        .pipe($.markdown())
+        .pipe($.minifyHtml({
+            empty: true,
+            spare: true,
+            quotes: true
+        }))
+        .pipe(gulp.dest('.tmp/content'));
+});
+
+gulp.task('styles', function () {
+    return gulp.src('app/styles/main.less')
+        .pipe($.less())
+        .pipe($.autoprefixer('last 1 version'))
+        .pipe(gulp.dest('.tmp/styles'));
 });
 
 gulp.task('images', function () {
@@ -133,7 +128,7 @@ gulp.task('images', function () {
 
 // fonts
 gulp.task('fonts', function () {
-    return gulp.src(['app/bower_components/**/fonts/*.{eot,otf,svg,ttf,woff}', 'app/bower_components/**/font/*.{eot,otf,svg,ttf,woff}'])
+    return gulp.src(['bower_components/**/fonts/*.{eot,otf,svg,ttf,woff}', 'bower_components/**/font/*.{eot,otf,svg,ttf,woff}'])
         .pipe($.flatten())
         .pipe(gulp.dest('dist/fonts'))
         .pipe($.size());
@@ -141,7 +136,7 @@ gulp.task('fonts', function () {
 
 // extras
 gulp.task('extras', function () {
-    return gulp.src(['app/*.*', 'app/**/*.json', 'app/**/*.mp3', 'app/**/*.webm', '!app/bower_components/**/*.json', '!app/bower_components/**/*.mp3', '!app/*.html', '!app/**/*.jade'], { dot: true })
+    return gulp.src(['app/**/*.json', 'app/**/*.mp3', 'app/**/*.webm'], { dot: true })
         .pipe(gulp.dest('dist'));
 });
 
@@ -152,14 +147,13 @@ gulp.task('clean', function () {
 });
 
 // build
-gulp.task('build', function() {
-    runSequence('styles', 'scripts', 'jade', 'html2js', 'html', 'pot', 'translations', 'images', 'fonts', 'extras');
+gulp.task('build', ['html', 'images', 'fonts', 'extras'], function() {
 });
 
 // connect
 gulp.task('connectDev', function () {
     $.connect.server({
-        root: ['app', '.tmp', 'app/bower_components/font-awesome'],
+        root: ['app', '.tmp', 'bower_components/font-awesome'],
         port: 8000,
         livereload: true
     });
@@ -173,29 +167,9 @@ gulp.task('connectDist', function () {
     });
 });
 
-// inject bower components
-gulp.task('wiredep', function () {
-    var wiredep = require('wiredep').stream;
-
-    // TODO: only change this in layout
-    gulp.src('app/layouts/default/*.jade')
-        .pipe(wiredep({
-            directory: 'app/bower_components'
-        }))
-        .pipe(gulp.dest('app'));
-});
-
 // watch
 gulp.task('watch', ['connectDev'], function () {
     // watch for changes
-
-    gulp.watch([
-        '.tmp/**/*.html',
-        '.tmp/styles/**/*.css',
-        '.tmp/scripts/**/*.js',
-        'app/scripts/**/*.js',
-        'app/images/**/*'
-    ]);
 
     gulp.watch('app/styles/**/*.less', ['styles']);
     gulp.watch('app/scripts/**/*.js', ['scripts']);
@@ -203,10 +177,10 @@ gulp.task('watch', ['connectDev'], function () {
     gulp.watch('app/content/**/*.md', ['jade']);
     gulp.watch('app/**/*.jade', ['jade']);
     gulp.watch('app/po/*.po', ['translations']);
-    gulp.watch('bower.json', ['wiredep']);
+    gulp.watch('bower.json', ['jade']);
     gulp.watch('gulpfile.js', ['build']);
 });
 
 //gulp.task('default', ['connectDist', 'connectDev', 'watch']);
 
-gulp.task('default', ['clean', 'build', 'watch']);
+gulp.task('default', ['build', 'watch']);
