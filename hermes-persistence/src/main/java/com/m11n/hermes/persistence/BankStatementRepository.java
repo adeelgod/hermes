12a5fd122f9364d94a6f0625a8453d17eddc2958
orce-mage <nikolovorce2@gmail.com>
2015-08-03 11:58:54 +0000

    Improve bank statement identity detection

--- hermes-persistence/src/main/java/com/m11n/hermes/persistence/BankStatementRepository.java
+++ hermes-persistence/src/main/java/com/m11n/hermes/persistence/BankStatementRepository.java
@@ -16,6 +16,9 @@ public interface BankStatementRepository extends PagingAndSortingRepository<Bank
 
     List<BankStatement> findByStatusAndAmountGreaterThan(String status, BigDecimal amount);
 
+    @Query("SELECT COUNT(*) FROM Auswertung.hermes_bank_statement bs where bs.account = :account AND bs.transfer_date = transfer_date AND bs.description_b = :description_b AND bs.amount = :amount AND bs.currency = :currency")
+    long exists(@Param("account") String account, @Param("transfer_date") Date transferDate, @Param("description_b") String descriptionB, @Param("amount") BigDecimal amount, @Param("currency") String currency);
+
     @Modifying(clearAutomatically = true)
     @Query("update BankStatement bs set bs.status =:status where bs.id =:id")
     void updateStatus(@Param("id") String id, @Param("status") String status);
