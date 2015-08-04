5118a041f6d6bc641a27eb60e626ebc19f206921
orce-mage <nikolovorce2@gmail.com>
2015-08-04 11:20:49 +0100

    Minor fix

--- hermes-persistence/src/main/java/com/m11n/hermes/persistence/BankStatementRepository.java
+++ hermes-persistence/src/main/java/com/m11n/hermes/persistence/BankStatementRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 @Repository
