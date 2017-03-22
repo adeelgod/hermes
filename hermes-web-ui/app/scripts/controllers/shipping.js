05a4c89da05083eb2efdff27d0ab484d56af6992
orce-mage <nikolovorce2@gmail.com>
2017-03-22 11:54:33 +0500

    updated weight value

--- hermes-web-ui/app/scripts/controllers/shipping.js
+++ hermes-web-ui/app/scripts/controllers/shipping.js
@@ -217,8 +217,8 @@
                 $scope.checks[shipping.orderId]['zip'] &&
                 $scope.checks[shipping.orderId].country &&
                 $scope.checks[shipping.orderId].dhlAccount);
-
-                shipping._selected = shipping.weight > 29 ? false: true;
+                //
+                shipping._selected = shipping.weight > 30 ? false: true;
             });
         };
 
